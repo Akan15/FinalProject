04337646f1820076d0af5e311fb969de4c90c476
Akan15 <71425960+Akan15@users.noreply.github.com>
2025-06-26 17:02:15 +0500

    Update NewsSection.jsx

--- frontend/src/components/sections/NewsSection.jsx
+++ frontend/src/components/sections/NewsSection.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useLanguage } from "../../context/LanguageContext";
 import "./NewsSection.css";
 
+const API_URL = "https://finalproject-2-w7he.onrender.com";
+
 const NewsSection = () => {
   const { language, t } = useLanguage();
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/news")
+    fetch("https://finalproject-2-w7he.onrender.com")
       .then(res => res.json())
       .then(data => setNews(data))
       .catch(() => setNews([]));
